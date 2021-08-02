3b1434d87363b77f2dd76297b1c608fd73b671a5
Purwa <purwadarozatunakis@gmail.com>
2021-08-02 11:32:55 +0700

    add fresh install php sampai composer

---
+++ scripts/installphpcert.sh
@@ -0,0 +1,1 @@
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg && echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list 
